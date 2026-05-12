a7ac3da2ec18a7e9067c155c732b17feb8378ad6
Hetart1508 <hetarth150804@gmail.com>
2026-05-12 11:17:54 +0530

    Update Calendar.tsx

--- src/pages/Calendar.tsx
+++ src/pages/Calendar.tsx
@@ -36,7 +36,6 @@ export default function CalendarView() {
     };
     fetchData();
   }, []);
-//Adding comment for showing git desktop changes
   const handleUpdateThreshold = async () => {
     try {
       await api.patch('/user/threshold', { threshold });
