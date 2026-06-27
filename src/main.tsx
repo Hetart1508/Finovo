781cfe05b9c0ebb1809ac2d871f5d4cea62550d3
Hetart1508 <hetarth150804@gmail.com>
2026-06-27 11:18:12 +0530

    Removed unecessary Style
    Same

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,6 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from './lib/queryClient';
-import './_metronic/assets/fonticon/fonticon.css';
-import './_metronic/assets/keenicons/duotone/style.css';
-import './_metronic/assets/keenicons/outline/style.css';
-import './_metronic/assets/keenicons/solid/style.css';
-import './_metronic/assets/sass/style.scss';
-import './_metronic/assets/sass/plugins.scss';
-import './_metronic/assets/sass/style.react.scss';
 import './index.css';
 
 const savedTheme = localStorage.getItem('theme');
